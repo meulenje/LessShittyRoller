b5e56849276f5c7532e2f5a8cc5c6fa17f32b468
Jeff Meulenberg <jeffmeulenberg@gmail.com>
2019-03-14 21:00:48 -0400

    Initial commit

---
+++ service.js
@@ -0,0 +1,20 @@
+var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+  name:'LessShittyRoller',
+  description: 'Roller bot for Exalted Discord',
+  script: 'D:\\Exalted Roller\\bot.js',
+  nodeOptions: [
+    '--harmony',
+    '--max_old_space_size=4096'
+  ]
+});
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on('install',function(){
+  svc.start();
+});
+
+svc.install();
